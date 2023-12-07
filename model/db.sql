4a88e5523ad39063e01d232b9430535c805c6611
Elias <130007507+EliasAraujoDev@users.noreply.github.com>
2023-12-07 10:42:10 -0300

    Update db.sql

--- model/db.sql
+++ model/db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE loja;
 USE loja;
-CREATE TABLE clientess (
+CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
