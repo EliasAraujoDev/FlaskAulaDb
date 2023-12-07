076c4dd7fc58ba347878d8a003750f8dc1359489
EliasAraujoDev <eliasaraujo1222@outlook.com>
2023-12-07 09:59:21 -0300

    atualização

--- app.py
+++ app.py
@@ -19,20 +19,40 @@ cursor = db.cursor()
 def index():
     return render_template('index.html')
 
+@app.route('/resultado')
+def resultado():
+    cursor.execute("SELECT * FROM clientes")
+    clientes = cursor.fetchall()
+    return render_template('resultado.html', clientes=clientes)
+
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar():
     nome = request.form['nome']
     email = request.form['email']
 
-    cursor.execute("INSERT INTO clientess (nome, email) VALUES (%s, %s)", (nome, email))
+    cursor.execute("INSERT INTO clientes (nome, email) VALUES (%s, %s)", (nome, email))
     db.commit()
 
-    cursor.execute("SELECT * FROM clientess")
-    clientess = cursor.fetchall()
+    cursor.execute("SELECT * FROM clientes")
+    clientes = cursor.fetchall()
 
-    mensagem = "Cadastro real"
+    mensagem = "Cadastro realizado"
 
-    return render_template('resultado.html', mensagem=mensagem, clientess=clientess)
+    return render_template('resultado.html', mensagem=mensagem, clientes=clientes)
+
+@app.route('/atualizar/<int:id>', methods=['GET', 'POST'])
+def atualizar(id):
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        cursor.execute("UPDATE clientes SET nome = %s, email = %s WHERE id = %s", (nome, email, id))
+        db.commit()
+
+        return redirect(url_for('resultado'))
+    cursor.execute("SELECT * FROM clientes WHERE id = %s", (id,))
+    cliente = cursor.fetchone()
+
+    return render_template('atualizar.html', cliente=cliente)
 
 if __name__ == '__main__':
     app.run(debug=True)
